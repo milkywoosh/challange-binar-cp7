91f2964726f0a74fd4c5a5ce8190621268e9375c
milkywoosh <archer.lukman@gmail.com>
2021-09-19 20:39:13 +0700

    add --opponen-- fields
     this field will inform which opponent they play with when playing,
    so the information can be seen by go to seeDashboard api

--- migrations/20210918070009-create-history.js
+++ migrations/20210918070009-create-history.js
@@ -17,6 +17,9 @@ module.exports = {
       result: {
         type: Sequelize.INTEGER
       },
+      opponent: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
